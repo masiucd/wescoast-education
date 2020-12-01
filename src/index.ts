491fe490d8731a81a887e7e9154ff62e145d82c3
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-12-01 12:20:13 +0100

    mvc2.0

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,4 @@
-import { Course } from "./school/data/Course"
-import { Teacher } from "./school/data/Teacher"
-import { Student } from "./school/data/Student"
-import { View } from "./school/view/View"
 import { nanoid } from "nanoid"
 ;(() => {
-  function controller() {
-    const mike = new Student(nanoid(), "Mike", "Szymkowiak", [])
-    const jon = new Student(nanoid(), "Jon", "Smith", [])
-    const clara = new Student(nanoid(), "Clara", "Sten", [])
-    const kim = new Student(nanoid(), "Kim", "Kallstrom", [])
-    const stina = new Student(nanoid(), "Stina", "Turner", [])
-
-    const boris = new Teacher(nanoid(), "Boris", "Einstein", [])
-    const linda = new Teacher(nanoid(), "Linda", "Smith", [])
-    const mia = new Teacher(nanoid(), "Mia", "Jonson", [])
-
-    const math = new Course(nanoid(), "math", [], boris)
-    const english = new Course(nanoid(), "english", [], linda)
-    const swedish = new Course(nanoid(), "swedish", [], mia)
-    const history = new Course(nanoid(), "history", [], boris)
-
-    math.addStudent(mike)
-    math.addStudent(jon)
-    english.addStudent(clara)
-    english.addStudent(stina)
-    history.addStudent(mike)
-    history.addStudent(stina)
-    swedish.addStudent(kim)
-    swedish.addStudent(stina)
-
-    const students = [mike, jon, clara, kim, stina]
-    const teachers = [boris, linda, mia]
-    const courses = [math, english, swedish, history]
-
-    const init = () => {
-      const v = new View(students, courses, teachers)
-      v.render()
-    }
-
-    return {
-      init,
-    }
-  }
-  controller().init()
+  //
 })()
---
+++ src/wescoast2.0/models/course.ts
@@ -0,0 +1,12 @@
+class Course {
+  id: string
+  title: string
+  about: string
+  duration: string
+  constructor(id: string, title: string, about: string, duration: string) {
+    this.id = id
+    this.title = title
+    this.about = about
+    this.duration = duration
+  }
+}
