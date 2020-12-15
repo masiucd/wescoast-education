188492c46cf6cd96e58a369732ac6e37ca59e05e
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-12-15 19:56:09 +0100

    with mediator

--- src/wescoast2.0/types.ts
+++
@@ -1,8 +0,0 @@
-import { Student } from "./models/student"
-import { Teacher } from "./models/teacher"
-import { Course } from "./models/course"
-
-export type Subject = "english" | "math" | "history" | "swedish"
-
-export type DisplayData = Course[] | Teacher[] | Student[]
-export type DisplayDataItem = Course | Teacher | Student
